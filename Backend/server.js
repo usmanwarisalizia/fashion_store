9bfe41ef972980d7ab87261caf6564c4db8214ed
usman waris <usmanwaris1999@gmail.com>
2025-12-11 19:31:51 +0500

    first committs

--- Backend/server.js
+++ Backend/server.js
@@ -2,21 +2,30 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import products from './api/products.js';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+
+    // eslint-disable-next-line no-undef
+    origin: process.env.FRONTEND_URL
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/products', (req, res) => {
     res.json(products);
 });
 
-const port = 3000;
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Backend Server is running on: http://localhost:${port}`);
 });
